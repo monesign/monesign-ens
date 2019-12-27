831b349959b1c60bd74a7871efc197989a854a41
Henry Syahputra <syahputra.hsp@gmail.com>
2019-12-27 17:54:42 +0800

    add basic test cases

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,36 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+const ENS = artifacts.require("ENSRegistry.sol");
+const FIFSRegistrar = artifacts.require("FIFSRegistrar.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
-};
+const web3 = new (require('web3'));
+const namehash = require('eth-ens-namehash');
+
+
+// Calculate root node hashes from the top of tld (top level domain)
+function getRootNodeFromTLD(tld) {
+    return {
+        namehash: namehash.hash(tld),
+        sha3: web3.sha3(tld)
+    };
+}
+
+// Deploy ENS and FIFSRegistrar
+async function deployFIFSRegistrar(deployer, tld) {
+    var rootNode = getRootNodeFromTLD(tld);
+    let ensInstance, registrarInstance;
+
+    deployer.deploy(ENS)
+        .then(function (instance) {
+            ensInstance = instance;
+            return deployer.deploy(FIFSRegistrar, ensInstance.address, rootNode.namehash);
+        }).then(function (instance) {
+        registrarInstance = instance;
+        return ensInstance.setSubnodeOwner('0x0', rootNode.sha3, registrarInstance.address);
+    }).then(function (receipt) {
+        console.log(`Status: ${receipt.receipt.status}`)
+    })
+}
+
+module.exports = function(deployer, network){
+  var tld = 'eth';
+  deployFIFSRegistrar(deployer, tld);
+}
