d121b1ca36883b20bb0d2445b2b264a864188c8d
Henry Syahputra <syahputra.hsp@gmail.com>
2019-12-27 16:57:54 +0800

    add dependencies

--- contracts/ConvertLib.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity >=0.4.21 <0.7.0;
-
-library ConvertLib {
-    function convert(uint amount,uint conversionRate) public pure returns (uint convertedAmount) {
-        return amount * conversionRate;
-    }
-}
